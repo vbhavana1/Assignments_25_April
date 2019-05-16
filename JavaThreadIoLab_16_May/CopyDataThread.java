2e6717782666a44df68e2339d0cea8f7f9b2e6e5
vbhavana1 <50025759+vbhavana1@users.noreply.github.com>
2019-05-17 05:14:12 +0530

    Add files via upload

---
+++ JavaThreadIoLab_16_May/CopyDataThread.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+class CopyDataThread extends Thread{
+	public void run(){
+		try{
+			
+			FileReader fr = new FileReader("source.txt");
+			BufferedReader br = new BufferedReader(fr);
+			FileWriter fw = new FileWriter("target.txt", true);
+			String str;
+			char ch[];
+			
+			while((str = br.readLine()) != null){
+
+				fw.write(str);
+				ch = str.toCharArray();
+				for ( int i = 1 ; i< 10; i++){
+					fw.write(ch);
+				Thread.sleep(2000);
+				System.out.println(" Keep delay of 5 seconds");
+			}
+		
+				fw.flush();
+				
+				
+			}
+			
+			br.close();
+			fw.close();
+			System.out.println("file copied");
+		}
+		catch(IOException e){
+			System.out.println(e);
+			// e.printStackTrace();
+
+		}
+		catch(InterruptedException a){
+			System.out.println(a);
+		}
+		}
+	public static void main(String[] args) {
+		Thread thread = new CopyDataThread();
+		thread.start();
+
+
+	}
+}
